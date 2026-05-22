2069ea3bd3ed761edc7403957c255de06a709689
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-05-22 10:43:05 -0500

    Correct model inheritance in Genre and Actor classes
    Fixed incorrect model inheritance for Genre and Actor classes.

--- db/models.py
+++ db/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 
-class Genre(models.models.Model):
+class Genre(models.Model):  # <-- Corregido aquí de models.models.Model a models.Model
     name = models.CharField(max_length=255)
 
 
-class Actor(models.models.Model):
+class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
