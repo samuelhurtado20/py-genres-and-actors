cb3345efe6d6a6c7063858aa10770f383852aec4
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-05-22 10:32:44 -0500

    Implement CRUD operations for Genre and Actor
    Added functionality to create, update, and delete genres and actors in the database. Implemented a main function that performs these operations and returns a filtered QuerySet of actors.

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-import init_django_orm  # noqa: F401
-
-from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
-def main() -> QuerySet:
-    pass
+def main():
+    # 1. Create
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+
+    # 2. Update
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(
+        last_name="Clooney"
+    )
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reves"
+    )
+
+    # 3. Delete
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    # 4. Return QuerySet filtered and ordered
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
