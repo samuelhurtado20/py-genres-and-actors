863fbe344b52acb2d9d73a91f5c059e4df0ce41b
samuelhurtado20 <samuelhurtado20@hotmail.com>
2026-05-26 18:39:30 -0500

    Refactor main function to use loops for creating Genre and Actor instances

--- main.py
+++ main.py
@@ -3,24 +3,20 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
+    genres = [("Western",), ("Action",), ("Drama",)]
+    for (name,) in genres:
+        Genre.objects.create(name=name)
 
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
-
-    Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(
-        last_name="Clooney"
-    )
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
-        first_name="Keanu", last_name="Reves"
-    )
+    actors = [
+        ("George", "Clooney"),
+        ("Keanu", "Reeves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first, last in actors:
+        Actor.objects.create(first_name=first, last_name=last)
 
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
